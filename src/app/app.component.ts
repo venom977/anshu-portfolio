c3db6d27347637fd0fb5266dd3241447610dcaba
amitjangirr977 <amitjangirr977@gmail.com>
2024-06-04 19:01:23 +0530

    new branch code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HeaderComponent, FooterComponent, ContentComponent],
+  template: `
+
+  <div [attr.data-bs-theme] = "theme">
+    <app-header/>
+    <app-content/>
+    <app-footer/>
+  </div>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'anshu-portfolio';
+  theme: string = "dark";
+
+  toggleTheme() {
+    this.theme = this.theme === "dark" ? "light" : "dark";
+  }
 }
