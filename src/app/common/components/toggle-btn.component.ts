fa85bb0e7832e458a52e9e8d23105ff56075643f
amitjangirr977 <amitjangirr977@gmail.com>
2024-06-16 13:31:22 +0530

    ui created

---
+++ src/app/common/components/toggle-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-toggle-btn',
+    standalone: true,
+    templateUrl: './toggle-btn.component.html',
+    styleUrl: './toggle-btn.component.scss'
+})
+
+export class ToggleBtnComponent { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ToggleBtnComponent } from '../common/components/toggle-btn.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ToggleBtnComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent { }
