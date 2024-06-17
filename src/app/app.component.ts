2c29811d4bf97bf7b0692d2d0c9afc09b0cbdcf8
amitjangirr977 <amitjangirr977@gmail.com>
2024-06-17 11:05:32 +0530

    new ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,19 @@ import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './common/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ContentComponent],
+  imports: [HeaderComponent, FooterComponent, ContentComponent, CarouselComponent],
   template: `
-
   <div [attr.data-bs-theme] = "theme">
     <app-header/>
     <app-content/>
     <app-footer/>
   </div>
+  
   `,
   styleUrl: './app.component.scss'
 })
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../common/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
 export class ContentComponent {
-  cardsContent = [1, 2, 3];
+  cardsContent = [
+    {
+      name: "",
+      src: "https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149052117.jpg?w=1380&t=st=1718546990~exp=1718547590~hmac=1cae21d4cda4fc85f90340b132e036b5f4a9c06a0000ee7fca16dfecace8e95d"
+    },
+    {
+      name: "",
+      src: "https://img.freepik.com/premium-photo/red-cell-phone-with-word-lg-screen_1020495-313224.jpg?w=1480"
+    },
+    {
+      name: "",
+      src: "https://img.freepik.com/premium-photo/avalanche-cryptocurrency-consensus-protocol-mobile-layout-wi-creative-idea-app-background-designs_1020495-314552.jpg?w=1380"
+    }
+  ];
 }
